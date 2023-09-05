c78c1ccc7ef8683b52459e284b8e42a265624e6f
raredead <smahelzed@gmail.com>
2023-09-05 22:45:33 +0700

    Разметка файлов для препроцесора и слайдер

---
+++ js/app.js
@@ -0,0 +1,19 @@
+// Slider
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
